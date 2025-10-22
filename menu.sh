dfdfd5e49f4f8599b349d645703e7a75701d14bf
antoniooo-beep <cancelaantonio10@gmail.com>
2025-10-22 14:38:13 -0300

    Scripts

---
+++ menu.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+menu_add() {
+    echo "                                                         "
+    echo "                                                         "
+    echo "---- Menu de manejo del sistema ----"
+    echo "1 .Opciones de directorios"
+    echo "2 .Opciones de archivos"
+    echo "3 .Opciones de usuarios"
+    echo "4 .Opciones de backup"
+    echo "5 .Opciones de restauracion de servicio"
+    echo "6 .Ver log del sistema y backups"
+    echo "7 .Ver configuraciones activas"
+    echo "8 .Salir del menu"   
+   
+   
+    read -p "Seleccione una opcion del menu: " opcion
+
+case $opcion in 
+    1)
+        source ./crud_directorios.sh
+    ;;
+
+    2)
+        source ./crud_archivos.sh
+    ;;
+
+    3)
+        source ./crud_usuarios.sh
+    ;;
+
+    4)
+        source ./tipos_backup.sh
+    ;;
+
+    .
+
+
+
+    .
+
+
+
+    .
+
+
+
+    8)exit ;; 
+
+
+
+    
+
+
+
+esac
+
+}
+while true; do
+menu_add
+done
+
