ce944c8f2d443d9ab1261a818c862e86b90d0dae
antoniooo-beep <cancelaantonio10@gmail.com>
2025-11-11 23:40:08 -0300

    PORFAVOR

--- useradd.sh
+++ useradd.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
- user_add(){
-        echo "ingrese datos para crear un usuario"
-        echo "                                        "
-        read -p "Ingresa el nombre del nuevo usuario" usuario
-        sudo adduser -m $usuario
-        sudo passwd $usuario
-        echo "                                        "
-        echo "usuario $usuario creado correctamente"
-    }
+user_add() {
+    echo "==============================================="
+    echo " Ingrese datos para crear un usuario"
+    echo "==============================================="
+    echo
+    read -p " Ingresa el nombre del nuevo usuario: " usuario
+
+    sudo adduser -m "$usuario"
+    
+    sudo passwd "$usuario"
+
+    echo
+    echo " Usuario '$usuario' creado correctamente."
+}
+
 user_add
